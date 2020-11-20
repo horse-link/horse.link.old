a01dc3826c2cd394fc416a1b462b734912408d70
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-11-21 08:23:04 +1000

    WIP

---
+++ contracts/Horse.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.7.0;
+
+contract Horse {
+
+    address private _owner;
+
+    mapping(bytes32 => mapping(uint => mapping(uint => mapping(uint => mapping(uint => uint[]))))) public results;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function addResult(bytes32 mnemonic, uint year, uint month, uint day, uint race, uint[] memory _results) public {
+        results[mnemonic][year][month][day][race].push(_results);
+    }
+
+    event Result();
+}
