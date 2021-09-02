e895dbb1f3dcd9dcb114964d12739a09ff22b9d7
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-09-02 21:31:14 +1000

    Update contract and scripts

--- contracts/Horse.sol
+++ contracts/Horse.sol
@@ -16,10 +16,10 @@ contract Horse is Ownable {
 
     uint256 public count;
 
-    mapping(address => mapping(bytes32 => mapping(uint256 => mapping(uint256 => mapping(uint256 => mapping(uint256 => uint256[])))))) public results;
+    mapping(bytes32 => mapping(uint256 => mapping(uint256 => mapping(uint256 => mapping(uint256 => uint256[]))))) public results;
 
     function addResult(bytes32 mnemonic, uint256 year, uint256 month, uint256 day, uint256 race, uint256[] memory _results) public {
-        results[msg.sender][mnemonic][year][month][day][race] = _results;
+        results[mnemonic][year][month][day][race] = _results;
         count++;
         emit ResultAdded(msg.sender, block.timestamp, mnemonic, year, month, day, race);
     }
--- scipts.py
+++ scipts.py
@@ -1,4 +1,7 @@
-from web3 import Web3
+# import urllib library
+from urllib.request import urlopen
+
+# from web3 import Web3
 import json
 
 from dotenv import load_dotenv
@@ -10,15 +13,21 @@ NODE = os.getenv('node')
 CONTRACT_ADDRESS = '0x95f7594Dc262bb6A969Aeb42E4D9E4BedA94FAa0' # os.getenv('address')
 print (NODE)
 
-w3 = Web3(Web3.WebsocketProvider(NODE))
+response = urlopen('https://api.beta.tab.com.au/v1/tab-info-service/racing/dates/2021-09-02/meetings?jurisdiction=QLD')
+data_json = json.loads(response.read())
+  
+# print the json response
+print(data_json)
 
-with open('./build/contracts/Horse.json') as json_file:
-  data = json.load(json_file)
-  abi=data['abi']
-  bytecode=data['bytecode']
+# w3 = Web3(Web3.WebsocketProvider(NODE))
 
-  # print(abi)
+# with open('./build/contracts/Horse.json') as json_file:
+#   data = json.load(json_file)
+#   abi=data['abi']
+#   bytecode=data['bytecode']
 
-  contract = w3.eth.contract(CONTRACT_ADDRESS, abi=abi)
-  count = contract.functions.count().call()
-  print (count)
+#   # print(abi)
+
+#   contract = w3.eth.contract(CONTRACT_ADDRESS, abi=abi)
+#   count = contract.functions.count().call()
+#   print (count)
